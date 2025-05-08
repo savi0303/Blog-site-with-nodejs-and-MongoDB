915b39a955bac14ac088a374559348d0726dbe1e
Savindu Weththasinghe <savindu.20230439@iit.ac.lk>
2025-05-08 21:27:12 +0530

    Pagination

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,14 +1,67 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
+const { parse } = require('dotenv');
 
 //Router
-router.get('', (req, res) => {
-    const locals = {
-        title : "NodeJs Blog",
-        description : "Simple Blog created with NodeJs, Express & MongoDB."
+router.get('', async(req, res) => {
+    try {
+        const locals = {
+            title : "NodeJs Blog",
+            description : "Simple Blog created with NodeJs, Express & MongoDB."
+        }
+
+        let perPage = 10;
+        let page = req.query.page || 1;
+
+        const data = await Post.aggregate([ {$sort: { createAt: -1 }}])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();
+
+        const count = await Post.countDocuments({});
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+        res.render('index', { 
+            locals, 
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null
+        });
+
+    } catch (error) {
+        console.log(error);
     }
-    res.render('index', { locals });
- });
+});
+
+//  router.get('', async(req, res) => {
+//     const locals = {
+//         title : "NodeJs Blog",
+//         description : "Simple Blog created with NodeJs, Express & MongoDB."
+//     }
+
+//     try {
+//         const data = await Post.find();
+//         res.render('index', { locals, data });
+//     } catch (error) {
+//         console.log(error);
+//     }
+
+//     res.render('index', { locals });
+//  });
+
+
+//  function insertPostData () {
+//     Post.insertMany([
+//         {
+//             title: "Building a Blog",
+//             body: "This is the body text"
+//         },
+//     ])
+//  }
+//  insertPostData();
+
 
 router.get('/about', (req, res) => {
     res.render('about');

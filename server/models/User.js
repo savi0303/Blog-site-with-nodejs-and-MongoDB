b7ea776ceeb2956e543f49ac5db7a29e1d6a4aa8
Savindu Weththasinghe <savindu.20230439@iit.ac.lk>
2025-05-10 03:40:24 +0530

    Admin and Dashboard

--- server/models/User.js
+++ server/models/User.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
-    Username: {
+    username: {
         type: String,
         required: true,
         unique: true
     },
-    Password: {
+    password: {
         type: String,
         required: true
     }

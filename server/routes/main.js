bd28d99277fb60ca02eb638d7bd9106f01cc1341
Savindu Weththasinghe <savindu.20230439@iit.ac.lk>
2025-05-09 17:07:33 +0530

    Search function

--- server/routes/main.js
+++ server/routes/main.js
@@ -72,20 +72,49 @@ router.get('', async(req, res) => {
  */
  router.get('/post/:id', async(req, res) => {
     try {
-        const locals = {
-            title : "NodeJs Blog",
-            description : "Simple Blog created with NodeJs, Express & MongoDB."
-        }
-
         let slug = req.params.id;
 
         const data = await Post.findById({ _id: slug });
+
+        const locals = {
+            title : data.title,
+            description : "Simple Blog created with NodeJs, Express & MongoDB."
+        }
+
         res.render('post', { locals, data });
     } catch (error) {
         console.log(error);
     }
  });
 
+ /**
+  * POST
+  * Post - searchTerm
+  */
+  router.post('/search', async(req, res) => {
+    try {
+        const locals = {
+            title : "Search",
+            description : "Simple Blog created with NodeJs, Express & MongoDB."
+        }
+
+        let searchTerm = req.body.searchTerm;
+        const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9]/g, "")
+
+        const data = await Post.find({
+            $or: [
+                { title: { $regex: new RegExp(searchNoSpecialChar, 'i') }},
+                { body: { $regex: new RegExp(searchNoSpecialChar, 'i') }}
+            ]
+        });
+        res.render("search", {
+            data,
+            locals
+        });
+    } catch (error) {
+        console.log(error);
+    }
+ });
 
 router.get('/about', (req, res) => {
     res.render('about');

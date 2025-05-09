30f73f32cc39b6272068906a6d7544fbdba79340
Savindu Weththasinghe <savindu.20230439@iit.ac.lk>
2025-05-09 21:02:42 +0530

    Admin page layout

---
+++ server/models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+    Username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    Password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);

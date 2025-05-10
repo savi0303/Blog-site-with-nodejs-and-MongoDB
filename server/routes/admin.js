ac0af3df078fd0eef18742299fa0d8b2bbe192b5
Savindu Weththasinghe <savindu.20230439@iit.ac.lk>
2025-05-10 16:05:18 +0530

    Add new post function

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -73,13 +73,71 @@ router.post('/admin', async(req, res) => {
 });
 
 /**
- * POST
+ * GET
  * Admin - Dashboard
  */
 router.get('/dashboard', authMiddleware, async(req, res) => {
-    res.render('admin/dashboard');
+    try {
+        const locals = {
+            title : "Dashboard",
+            description : "Simple Blog created with NodeJs, Express & MongoDB."
+        }
+
+        const data = await Post.find();
+        res.render('admin/dashboard', {
+            locals,
+            data,
+            layout: adminLayout
+        });
+    } catch (error) {
+        console.log(error);
+    }
 });
 
+/**
+ * GET
+ * Admin - Create New Post
+ */
+router.get('/add-post', authMiddleware, async(req, res) => {
+    try {
+        const locals = {
+            title : "Add Post",
+            description : "Simple Blog created with NodeJs, Express & MongoDB."
+        }
+
+        const data = await Post.find();
+        res.render('admin/add-post', {
+            locals,
+            layout: adminLayout
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+/**
+ * POST
+ * Admin - Create New Post
+ */
+router.post('/add-post', authMiddleware, async(req, res) => {
+    try {
+        try {
+            const newPost = new Post({
+                title: req.body.title,
+                body: req.body.body
+            });
+
+            await Post.create(newPost);
+            res.redirect('/dashboard');
+        } catch (error) {
+            console.log(error);
+        }
+
+        res.redirect('/dashboard');
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 // router.post('/admin', async(req, res) => {
 //     try {
